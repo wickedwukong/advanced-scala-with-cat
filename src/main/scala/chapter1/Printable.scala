dbe85d00d17b9ae2c023a6cbb9e3abd51cf83278
Xuemin Guan <gxuemin@gmail.com>
2016-10-02 21:10:30 +0100

    implmented Pritnable for cat using both inteface methods and syntax

--- src/main/scala/chapter1/Printable.scala
+++ src/main/scala/chapter1/Printable.scala
@@ -20,10 +20,40 @@ object Print {
   def print[A](input: A)(implicit printer: Printable[A]): Unit = { println(format(input))}
 }
 
-object Test extends App {
+final case class Cat(name: String, age: Int, color: String)
+
+object Cat {
+  import PrintDefaults._
+  implicit val catPrintable = new Printable[Cat] {
+    override def format(cat: Cat): String = {
+      val name = Print.format(cat.name)
+      val age = Print.format(cat.age)
+      val color = Print.format(cat.color)
+
+      s"$name is a $age year-old $color cat."
+    }
+  }
+
+}
+
+object PrintSyntax {
+  implicit class PrintOps[A](value: A) {
+    def format(implicit printable: Printable[A]): String = printable.format(value)
+    def print(implicit printable: Printable[A]): Unit = println(printable.format(value))
+  }
+}
+
+object Main extends App {
   import Print._
   import PrintDefaults._
   import Print._
+  import PrintSyntax._
 
   Print.print(1)
+
+   val cat = Cat("Mason", 100, "black")
+
+  print(cat)
+
+  cat.print
 }
