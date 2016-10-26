b08247e057eb15fc98cf84440f7ee7197a41d901
Xuemin Guan <gxuemin@gmail.com>
2016-10-26 21:59:38 +0100

    finished functor exercise

--- src/main/scala/chapter3/FunctorDemo.scala
+++ src/main/scala/chapter3/FunctorDemo.scala
@@ -1,23 +1,37 @@
 package chapter3
 
-trait Functor[F[_]] {
-  def map[A, B](a: F[A])(f: A => B): F[B]
-}
+object FunctorDemo extends App {
 
-object FunctorDemo {
+  sealed trait Result[+A]
 
-  sealed trait Result[+A] // defined trait Result
-  final case class Success[A](value: A) extends Result[A] // defined class Success
-  final case class Warning[A](value: A, message: String) extends Result[A] // defined class Warning
+  // defined trait Result
+  final case class Success[A](value: A) extends Result[A]
+
+  final case class Warning[A](value: A, message: String) extends Result[A]
+
   final case class Failure(message: String) extends Result[Nothing]
 
-  val resultFunctor = new Functor[Result] {
-    override def map[A, B](a: Result[A])(f: A => B): Result[B] = {
-      a match {
-        case Success(value) => Success(f(value))
-        case Warning(value, message) => Warning(f(value), message)
+  def success[A](value: A): Result[A] = Success(value)
+
+  def warning[A](value: A, message: String): Result[A] = Warning(value, message)
+
+  def failure[A](message: String): Result[A] = Failure(message)
+
+  import cats.Functor
+  import cats.syntax.functor._
+
+  implicit val resultFunctor = new Functor[Result] {
+    def map[A, B](result: Result[A])(func: A => B): Result[B] =
+      result match {
+        case Success(value) => Success(func(value))
+        case Warning(value, message) => Warning(func(value), message)
         case Failure(message) => Failure(message)
       }
-    }
   }
+
+//  Success(100) map (_ * 2)
+
+  val success200 = success(100) map (_ * 2)
+
+  println(success200)
 }
