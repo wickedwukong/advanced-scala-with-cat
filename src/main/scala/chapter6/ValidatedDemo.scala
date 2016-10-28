78e772e02904e0e21fcf787693e2c1d10826d765
Xuemin Guan <gxuemin@gmail.com>
2016-10-28 22:51:52 +0100

    added an example of using foldLeft to accumulate an a list of both error and success

--- src/main/scala/chapter6/ValidatedDemo.scala
+++ src/main/scala/chapter6/ValidatedDemo.scala
@@ -1,5 +1,7 @@
 package chapter6
 
+import cats.data.Validated.{Invalid, Valid}
+
 object ValidatedDemo extends App {
   import cats.data.Validated
   import cats.instances.list._
@@ -18,6 +20,16 @@ object ValidatedDemo extends App {
 
 
   println(threeValidedString.tupled)
+
+  val x = List(Validated.invalid[String, String]("Fail1"),Validated.invalid[String, String]("Fail2"), Validated.valid[String, String]("Success1"))
+    .foldLeft((List.empty[String], List.empty[String])){
+    (valid, validated) => validated match {
+      case Invalid(message) => (message :: valid._1, valid._2)
+      case Valid(checkName) => (valid._1, checkName :: valid._2)
+    }
+  }
+
+  println(x)
 
   val isItAllValid = List(Validated.invalid("Fail1"),Validated.invalid("Fail2")).forall(_.isValid)
 
