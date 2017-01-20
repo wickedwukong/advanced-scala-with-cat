d8191fe149c97b854ff6692c23fe73f6ea478ac2
Xuemin Guan <x.guan@elsevier.com>
2017-01-20 15:21:35 +0000

    Added thread id to make demo writer monad does not have interleaving log message

--- src/main/scala/chatper4/SlowFactorial.scala
+++ src/main/scala/chatper4/SlowFactorial.scala
@@ -3,6 +3,7 @@ package chatper4
 import cats.Id
 import cats.data.{Writer, WriterT}
 
+import scala.collection.immutable.Seq
 import scala.concurrent.duration.Duration
 
 object SlowFactorial {
@@ -14,7 +15,7 @@ object SlowFactorial {
   def factorial(n: Int): Int = {
     val result = slowly(if (n == 0) 1 else n * factorial(n - 1))
 
-    println(s"n is: $n result is: $result")
+    println(s"Thread: ${Thread.currentThread().getId} n is: $n result is: $result")
 
     result
   }
@@ -53,7 +54,7 @@ object SlowFactorialWithWriterMonad {
       for {
         n <- loggedFactorial
         result <- slowly(if (n == 0) 1.pure[Logged] else factorial(n - 1).map(_ * n))
-        _ <- Vector(s"n is: $n result is: $result").tell
+        _ <- Vector(s"Thread: ${Thread.currentThread().getId} n is: $n result is: $result").tell
       } yield result
     }
 
@@ -66,7 +67,7 @@ object SlowFactorialWithWriterMonad {
     } else {
       for {
         fact <- slowly(factorial2(n -1))
-        _ <- Vector(s"n is: $n result is: $fact").tell
+        _ <- Vector(s"Thread: ${Thread.currentThread().getId} n is: $n result is: $fact").tell
       } yield {n * fact}
     }
   }
@@ -83,9 +84,9 @@ object WriterMonadDemoWithWriterMonadApp extends App {
 
   val results: List[Logged[Int]] = Await.result(futureFactorials, Duration.Inf)
 
+
   results.map(_.run).foreach{ case (messages, result) => {
-    println(s"logs: $messages")
-    println(s"result: $result")
+    println(s"logs: ${messages.mkString(" ")} result: $result")
   }}
 
   var futureFactorials2: Future[List[Logged[Int]]] = Future.sequence(List(Future(factorial2(5)), Future(factorial2(5))))
@@ -93,8 +94,7 @@ object WriterMonadDemoWithWriterMonadApp extends App {
   val results2: List[Logged[Int]] = Await.result(futureFactorials2, Duration.Inf)
 
   results2.map(_.run).foreach{ case (messages, result) => {
-    println(s"logs: $messages")
-    println(s"result: $result")
+    println(s"logs: ${messages.mkString(" ")} result: $result")
   }}
 
 }
