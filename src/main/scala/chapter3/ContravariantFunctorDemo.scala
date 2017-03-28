e68d3826f23688904de1806638f431180480532c
Xuemin Guan <gxuemin@gmail.com>
2017-03-28 22:37:32 +0100

    Added a contramap solution for Box

--- src/main/scala/chapter3/ContravariantFunctorDemo.scala
+++ src/main/scala/chapter3/ContravariantFunctorDemo.scala
@@ -12,9 +12,28 @@ trait Printable[A] {
 
 object ContravariantFunctorDemo extends App {
 
+  implicit val stringPrintable =
+    new Printable[String] {
+      def format(value: String): String =
+        "\"" + value + "\""
+    }
+
+  implicit val booleanPrintable =
+    new Printable[Boolean] {
+      def format(value: Boolean): String =
+        if(value) "yes" else "no"
+    }
+
   def format[A](value: A)(implicit p: Printable[A]): String =
     p.format(value)
 
+  println(s"format hello: ${format("hello")}")
+  println(s"format true: ${format(true)}")
 
+  final case class Box[A](value: A)
 
+  implicit def boxPrintable[A](implicit printable: Printable[A]): Printable[Box[A]] = printable.contramap(box => box.value)
+
+  println(s"format Box hello: ${format(Box("hello"))}")
+  println(s"format Box true: ${format(Box(true))}")
 }
