bec9532d780935c1dc14ea22ad85c8fa89c73d01
Xuemin Guan <x.guan@elsevier.com>
2017-05-19 12:04:02 +0100

    implemented Box codec using imap. XG

--- src/main/scala/chapter3/InvariantFunctorDemo.scala
+++ src/main/scala/chapter3/InvariantFunctorDemo.scala
@@ -16,6 +16,8 @@ trait Codec[A] {
   }
 }
 
+case class Box[A](value: A)
+
 object InvariantFunctorDemo extends App {
 
   implicit val intCodec: Codec[Int] = new Codec[Int] {
@@ -27,4 +29,10 @@ object InvariantFunctorDemo extends App {
 
   println(s"decoding 1: ${intCodec.decode("1")}")
   println(s"decoding bad integer: ${intCodec.decode("bad integer")}")
+
+  def boxCodec[A](implicit codecA: Codec[A]): Codec[Box[A]] = codecA.imap(Box(_), _.value)
+
+  println(s"encode Box(123) to 123 : ${boxCodec.encode(Box[Int](123))}")
+  println(s"decode 123 to Box(123): ${boxCodec.decode("123")}")
+
 }
